1667f72c433a3f32c91a94d04a98c061e11bc5b8
AndreChavs <keysandr7@gmail.com>
2021-05-17 15:19:12 -0300

    alteração no menu principal

--- js/menu-scroll.js
+++ js/menu-scroll.js
@@ -1,5 +1,7 @@
 window.addEventListener('scroll', function () {
     let header = document.querySelector('header');  //variável local que recebe a seleção do elemento header do documento HTML pelo API do DOM.
+    let principal = document.querySelector('.principal')
     let windowPosition = window.scrollY > 80;    //variável local que recebe a posição da janela de scroll na posição Y (vertical) > 0
     header.classList.toggle('scrolling-active', windowPosition);
+    principal.classList.toggle('scrolling-active',windowPosition);   
 })
